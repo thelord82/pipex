6e0a8b18234894d6fd67ba664451ba428cb66142
thelord82 <mandrelord@gmail.com>
2022-08-02 11:52:16 -0400

    Little work in progress

--- pipex.c
+++ pipex.c
@@ -6,7 +6,41 @@
 /*   By: malord <malord@student.42quebec.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 09:46:10 by malord            #+#    #+#             */
-/*   Updated: 2022/07/07 10:08:26 by malord           ###   ########.fr       */
+/*   Updated: 2022/08/02 10:15:55 by malord           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "pipex.h"
+
+int	main(void)
+{
+	int		pipefds[2];
+	char	*pin;
+	char	*buffer;
+	pid_t	pid;
+
+	if (pipe(pipefds) == -1)
+	{
+		perror("pipe");
+		exit(0);
+	}
+	pid = fork();
+	if (pid == 0) // child process = 0, parent process > 0
+	{
+		pin = "69696969";
+		close (pipefds[0]);
+		write (pipefds[1], pin, ft_strlen(pin));
+		printf("Generating PIN in child and sending to parent ...\n");
+		exit(0);
+	}
+	buffer = ft_calloc(sizeof(char), 8);
+	if (pid > 0) // parent process! 
+	{
+		waitpid(pid, NULL, 0);
+		close (pipefds[1]);
+		read(pipefds[0], buffer, 8);
+		close (pipefds[0]);
+		printf("Parent received pin '%s'\n", buffer);
+	}
+	return (0);
+}
---
+++ pipex.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: malord <malord@student.42quebec.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/11 10:39:45 by malord            #+#    #+#             */
+/*   Updated: 2022/08/01 11:41:37 by malord           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+# include <sys/types.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include "libft/libft.h"
+#endif
